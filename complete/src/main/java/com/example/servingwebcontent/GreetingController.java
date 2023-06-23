b1b090323547f94ced50f3595bc41c2af2ddbafa
alinahid477 <ali@mint.com>
2023-06-23 09:24:10 +0000

    added url test instead

--- complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -2,10 +2,12 @@ package com.example.servingwebcontent;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@CrossOrigin
 public class GreetingController {
 
 	@GetMapping("/greeting")
--- complete/src/test/java/com/example/servingwebcontent/ServingWebContentApplicationTest.java
+++ complete/src/test/java/com/example/servingwebcontent/ServingWebContentApplicationTest.java
@@ -17,38 +17,56 @@
 package com.example.servingwebcontent;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = GreetingController.class)
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+
+//@WebMvcTest(controllers = GreetingController.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ServingWebContentApplicationTest {
 
+	// @Autowired
+	// private MockMvc mockMvc;
+
+	@Value(value = "${local.server.port}")
+	private int port;
+
 	@Autowired
-	private MockMvc mockMvc;
+	private TestRestTemplate restTemplate;
 
 	@Test
 	public void homePage() throws Exception {
 		// N.B. jsoup can be useful for asserting HTML content
-		mockMvc.perform(get("/index.html"))
-				.andExpect(content().string(containsString("Simple contents")));
+		// mockMvc.perform(get("/index.html"))
+		// 		.andExpect(content().string(containsString("Simple contents")));
+		assertTrue(this.restTemplate.getForObject("http://localhost:"+port+"/", String.class).contains("Simple contents"), "response expected to contain 'Simple contents' received: "+this.restTemplate.getForObject("http://localhost:"+port+"/greeting?name=Tanzu", String.class));
 	}
 
 	@Test
 	public void greeting() throws Exception {
-		mockMvc.perform(get("/greeting"))
-				.andExpect(content().string(containsString("Hello, World!")));
+		// mockMvc.perform(get("/greeting"))
+		// 		.andExpect(content().string(containsString("Hello, World!")));
+		assertTrue(this.restTemplate.getForObject("http://localhost:"+port+"/greeting", String.class).contains("Hello, World!"), "response expected to contain 'Hello, World!' received: "+this.restTemplate.getForObject("http://localhost:"+port+"/greeting?name=Tanzu", String.class));
 	}
 
 	@Test
 	public void greetingWithUser() throws Exception {
-		mockMvc.perform(get("/greeting").param("name", "Greg"))
-				.andExpect(content().string(containsString("Hello, Greg!")));
+		// mockMvc.perform(get("/greeting").param("name", "Ali"))
+		// 		.andExpect(content().string(containsString("Hello, Ali!")));
+		assertTrue(this.restTemplate.getForObject("http://localhost:"+port+"/greeting?name=Ali", String.class).contains("Hello, Ali!"), "response expected to contain 'Hello, Ali!' received: "+this.restTemplate.getForObject("http://localhost:"+port+"/greeting?name=Tanzu", String.class));
 	}
 
 }
